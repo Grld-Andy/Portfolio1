752fbec85e2eb351d027e1c4fcf90ea863305548
Andy Ansong <andyansong82@gmail.com>
2025-05-12 05:58:31 +0100

    feat hero🦸‍♂️

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Hero from '../components/HomeUI/Hero'
 
 const Home: React.FC = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <Hero/>
+    </div>
   )
 }
 
