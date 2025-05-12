a09901092d09022e41109125f32c2f3d5f4f50c7
Andy Ansong <andyansong82@gmail.com>
2025-05-12 08:23:43 +0100

    Update ToolsWheel.tsx

--- src/components/HomeUI/ToolsWheel.tsx
+++ src/components/HomeUI/ToolsWheel.tsx
@@ -5,21 +5,21 @@ const ToolsWheel: React.FC = () => {
     <div className='md:mt-52 mt-20'>
       <div className='flex items-center justify-center relative'>
         <div className='scale-100 md:scale-125 rounded-full w-[600px] h-[600px] flex items-center justify-center relative'>
-          <img className='w-14 h-14 absolute top-[-30px]' src='/public/icons/itunes.png'/>
-          <img className='w-14 h-14 absolute top-[15px] right-[125px] rounded-lg' src='/public/icons/github.png'/>
-          <img className='w-14 h-14 absolute top-[125px] right-[15px] rounded-lg' src='/public/icons/x.png'/>
+          <img className='w-14 h-14 absolute top-[-30px]' src='/icons/itunes.png'/>
+          <img className='w-14 h-14 absolute top-[15px] right-[125px] rounded-lg' src='/icons/github.png'/>
+          <img className='w-14 h-14 absolute top-[125px] right-[15px] rounded-lg' src='/icons/x.png'/>
 
-          <img className='w-14 h-14 absolute left-[-30px]' src='/public/icons/Figma icon.svg'/>
-          <img className='w-14 h-14 absolute bottom-[15px] left-[125px] rounded-lg' src='/public/icons/ps.png'/>
-          <img className='w-14 h-14 absolute bottom-[125px] left-[15px] rounded-lg' src='/public/icons/OBS.png'/>
+          <img className='w-14 h-14 absolute left-[-30px]' src='/icons/Figma icon.svg'/>
+          <img className='w-14 h-14 absolute bottom-[15px] left-[125px] rounded-lg' src='/icons/ps.png'/>
+          <img className='w-14 h-14 absolute bottom-[125px] left-[15px] rounded-lg' src='/icons/OBS.png'/>
 
-          <img className='w-14 h-14 absolute bottom-[-30px]' src='/public/icons/Coding.png'/>
-          <img className='w-14 h-14 absolute bottom-[15px] right-[125px] rounded-lg' src='/public/icons/Design.png'/>
-          <img className='w-14 h-14 absolute bottom-[125px] right-[15px] rounded-lg' src='/public/icons/framer.png'/>
+          <img className='w-14 h-14 absolute bottom-[-30px]' src='/icons/Coding.png'/>
+          <img className='w-14 h-14 absolute bottom-[15px] right-[125px] rounded-lg' src='/icons/Design.png'/>
+          <img className='w-14 h-14 absolute bottom-[125px] right-[15px] rounded-lg' src='/icons/framer.png'/>
 
-          <img className='w-14 h-14 absolute right-[-30px]' src='/public/icons/illustrator.png'/>
-          <img className='w-14 h-14 absolute top-[15px] left-[125px] rounded-lg' src='/public/icons/Photoshop.png'/>
-          <img className='w-14 h-14 absolute top-[125px] left-[15px] rounded-lg' src='/public/icons/Group 2038.png'/>
+          <img className='w-14 h-14 absolute right-[-30px]' src='/icons/illustrator.png'/>
+          <img className='w-14 h-14 absolute top-[15px] left-[125px] rounded-lg' src='/icons/Photoshop.png'/>
+          <img className='w-14 h-14 absolute top-[125px] left-[15px] rounded-lg' src='/icons/Group 2038.png'/>
         </div>
         <h2 className='text_gradient0 absolute font-extrabold text-[55px] md:text-[75px] uppercase text-center'>
           Let's create<br/>
