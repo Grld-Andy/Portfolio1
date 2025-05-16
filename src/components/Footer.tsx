5712f742e210c439dab478ecb7a8097960c46e0e
Andy Ansong <andyansong82@gmail.com>
2025-05-16 20:10:31 +0100

    push

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,10 +4,10 @@ const Footer: React.FC = () => {
   return (
     <div className='border-t-[1px] border-[#75757A] mt-10 mb-10 md:mb-0 px-10 flex justify-between flex-col md:flex-row'>
         <div>
-            <h1 className='jedira text-[200px]'>ed.win</h1>
-            <h1 className='text-[#75757A] text-[10px] relative bottom-10 left-5'>@2025 ed.win</h1>
+            <h1 className='jedira relative left-[-20px] text-[100px] md:text-[200px]'>ed.win</h1>
+            <h1 className='text-[#75757A] text-[10px] relative bottom-5 left-[-15px] md:bottom-10 md:left-5'>@2025 ed.win</h1>
         </div>
-        <div className='flex gap-20 mt-5 md:mr-14 justify-end'>
+        <div className='flex gap-10 md:gap-20 mt-5 md:mr-14 justify-end'>
             <section>
                 <h3 className='font-bold'>Links</h3>
                 <ul className='flex flex-col gap-4'>
--- src/components/ui/HomeUI/Hero.tsx
+++ src/components/ui/HomeUI/Hero.tsx
@@ -6,7 +6,7 @@ const Hero: React.FC = () => {
         <div className='w-full mt-20 flex gap-8 items-center justify-center flex-col'>
             <div className='flex items-center gap-5'>
                 <FourBars/>
-                <h1 className='text-[95px] font-semibold relative bottom-2'>click To Explore</h1>
+                <h1 className='text-[50px] md:text-[95px] font-semibold relative bottom-2'>click To Explore</h1>
                 <FourBarsReverse/>
             </div>
             <div className='flex gap-[200px] md:gap-[530px] justify-between items-center text-[#75757A] text-[15px] uppercase'>
